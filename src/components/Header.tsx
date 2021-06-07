7f8f85f77134b5272a8a38057494a08fc8a5bf41
nesrine mrabet <mrabet_nesrine@yahoo.fr>
2021-06-07 15:53:57 +0100

    Setting up the navbar menu

---
+++ src/components/Header.tsx
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import { IoIosArrowForward } from "react-icons/io";
+
+export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  console.log(isOpen);
+  return (
+    <div>
+      <div className="mt-4 mx-4 flex justify-between items-center pb-4 md:flex-col xl:flex-row xl:justify-between">
+        <h1 className="text-white text-2xl">THE PLANETS</h1>
+        <button
+          type="button"
+          className="focus:outline-none block md:hidden"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <svg
+            className="h-6 w-6 fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            {isOpen && (
+              <path
+                fill="#FFF"
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
+              />
+            )}
+            {!isOpen && (
+              <path
+                fill="#FFF"
+                fillRule="evenodd"
+                d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+              />
+            )}
+          </svg>
+        </button>
+        <nav className="hidden md:block md:mt-6 xl:mt-0">
+          <ul className="text-white flex space-x-8 font-bold">
+            <li>
+              MERCURY{" "}
+              <span className="md:hidden">
+                <IoIosArrowForward />
+              </span>
+            </li>
+            <li>
+              VENUS{" "}
+              <span className="md:hidden">
+                <IoIosArrowForward />
+              </span>
+            </li>
+            <li>
+              EARTH{" "}
+              <span className="md:hidden">
+                <IoIosArrowForward />
+              </span>
+            </li>
+            <li>
+              MARS{" "}
+              <span className="md:hidden">
+                <IoIosArrowForward />
+              </span>
+            </li>
+            <li>
+              JUPITER{" "}
+              <span className="md:hidden">
+                <IoIosArrowForward />
+              </span>
+            </li>
+            <li>
+              SATURNE{" "}
+              <span className="md:hidden">
+                <IoIosArrowForward />
+              </span>
+            </li>
+            <li>
+              URANUS{" "}
+              <span className="md:hidden">
+                <IoIosArrowForward />
+              </span>
+            </li>
+            <li>
+              NEPTUNE{" "}
+              <span className="md:hidden">
+                <IoIosArrowForward />
+              </span>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <hr className="opacity-30" />
+    </div>
+  );
+}
