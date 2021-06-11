5cefd2e8dc49d610e7be60f75860ace4e73f3477
nesrine mrabet <mrabet_nesrine@yahoo.fr>
2021-06-11 11:18:19 +0100

    Add dynamic content

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
 import "./App.css";
 import Header from "./components/Header";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Mercury from "./pages/Mercury";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Planet from "./components/Planet";
+import data from "./data/data.json";
+
+const loadedData = JSON.stringify(data);
+const json = JSON.parse(loadedData);
 
 function App() {
-  return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route exact path={"/mercury"} component={Planet}/>
-      </Switch>
-    </Router>
-  );
+  console.log(json);
+  return <div>
+    <Header />
+     {json.map((detail) => <Planet name={detail.name} content={detail.overview.content} source={detail.overview.source} />)}
+  </div>;
 }
 
 export default App;
+
+// {
+//   /* <Router>
+//       <Header />
+//       <Switch>
+//         <Route exact path={"/mercury"} component={Planet}/>
+//       </Switch>
+//     </Router> */
+// }
